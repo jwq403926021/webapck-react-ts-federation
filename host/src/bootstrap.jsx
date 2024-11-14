1421823fc50fa2cc24d7b2c227c0f1055980e011
JiangWenQi-Carl <403926021@qq.com>
2024-11-14 14:00:47 +0800

    init - add react-i18n

--- host/src/bootstrap.jsx
+++ host/src/bootstrap.jsx
@@ -4,6 +4,7 @@ import './scss/normalize.css'
 import './scss/index.css'
 import App from './App.tsx'
 import {createTheme, StyledEngineProvider, ThemeProvider} from "@mui/material";
+import './locales/i18n.js'
 const defaultTheme = createTheme();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- host/src/pages/Home.tsx
+++ host/src/pages/Home.tsx
@@ -1,8 +1,18 @@
+import {useTranslation} from "react-i18next";
+
 export interface HomeProps {}
 const Home: React.FC<HomeProps> = () => {
+  const { t, i18n } = useTranslation();
+
+  function switchLng() {
+    console.log(i18n.language);
+    i18n.changeLanguage(i18n.language === 'en' ? 'jp' : 'en')
+  }
+
   return (
     <div>
-      Home
+      <button onClick={switchLng}>switchLng</button>
+      Home: {t('welcome')}
     </div>
   );
 };

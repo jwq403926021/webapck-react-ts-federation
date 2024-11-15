577fe4d60e54b125b46d1c6fccdcdb1993167415
JiangWenQi-Carl <403926021@qq.com>
2024-11-15 10:07:57 +0800

    init

--- micro-app1/configs/webpack.prod.cjs
+++ micro-app1/configs/webpack.prod.cjs
@@ -1,11 +1,10 @@
-const path = require('path');
 const { merge } = require('webpack-merge');
 const webpackBaseConfig = require('./webpack.base.cjs');
 
 // @ts-ignore
 module.exports = merge(webpackBaseConfig, {
   mode: 'production',
-  devtool: 'cheap-source-map',
+  devtool: 'source-map',
 
   cache: true,
   optimization: {

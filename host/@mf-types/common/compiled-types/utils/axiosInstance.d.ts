6692c428808c2d5ce82232903864d7aa5989e80c
JiangWenQi-Carl <403926021@qq.com>
2024-11-28 13:34:10 +0800

    upgrade package

--- host/@mf-types/common/compiled-types/utils/axiosInstance.d.ts
+++ host/@mf-types/common/compiled-types/utils/axiosInstance.d.ts
@@ -1,4 +1,4 @@
-import { AxiosRequestConfig } from 'axios';
-export declare const $get: (url: string, params?: any, options?: AxiosRequestConfig) => Promise<import("axios").AxiosResponse<any, any>>;
-export declare const $post: (url: string, params?: any, options?: AxiosRequestConfig) => Promise<import("axios").AxiosResponse<any, any>>;
-export declare const $postForm: (url: string, params?: any, options?: AxiosRequestConfig) => Promise<import("axios").AxiosResponse<any, any>>;
+import Axios, { AxiosRequestConfig } from 'axios';
+export declare const $get: (url: string, params?: any, options?: AxiosRequestConfig) => Promise<Axios.AxiosResponse<any, any>>;
+export declare const $post: (url: string, params?: any, options?: AxiosRequestConfig) => Promise<Axios.AxiosResponse<any, any>>;
+export declare const $postForm: (url: string, params?: any, options?: AxiosRequestConfig) => Promise<Axios.AxiosResponse<any, any>>;
